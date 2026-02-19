51ac1ca5feb3d0c42e6a088b55fc6bf42436c78d
andrew <sigeyandrew10@gmail.com>
2026-02-19 20:35:09 +0300

    added users to models init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,5 +5,6 @@ Import all models here for easy access.
 from app.models.room import Room
 from app.models.inquiry import Inquiry
 from app.models.event_inquiry import EventInquiry
+from app.models.user import User 
 
-__all__ = ['Room', 'Inquiry', 'EventInquiry']
+__all__ = ['Room', 'Inquiry', 'EventInquiry', 'User']
